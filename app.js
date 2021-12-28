3e3f94163517b8f3c236a131a170e083325e684c
usa3amustafa <usa3a3ustafa@outlook.com>
2021-12-28 18:45:46 +0500

    images updated

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const header = document.querySelector('#welcome')
+// const header = document.querySelector('#welcome')
 const menu = document.querySelector('#menu')
 const nav = document.querySelector('.nav')
 const link = document.querySelectorAll('.link')
 const links = document.querySelector('.links')
+const header = document.querySelector('.header-section')
 
+// toggle hamburger
 menu.addEventListener('click', function () {
   links.classList.toggle('show-links')
 })
 
-const initialCoords = header.getBoundingClientRect()
+// stickyNav
+const stickyNav = function (entries) {
+  const [entry] = entries
+  console.log(entry)
+  if (!entry.isIntersecting) nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
 
-console.log(initialCoords.y)
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > initialCoords.y) {
-    nav.classList.add('sticky')
-    link.forEach(l => (l.style.color = '#878787'))
-    menu.style.color = '#878787'
-  } else {
-    nav.classList.remove('sticky')
-    link.forEach(l => (l.style.color = '#fef8ff'))
-    menu.style.color = '#fef8ff'
-  }
+const headerobserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
 })
 
+headerobserver.observe(header)
+
+// hide navbar on click
 link.forEach(l =>
   l.addEventListener('click', () => {
     links.classList.toggle('show-links')
